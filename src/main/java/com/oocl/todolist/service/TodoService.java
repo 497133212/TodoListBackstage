faf54a250bbaf79bc6b701eef63833914b1a2a1a
Austin <497133212@qq.com>
2020-08-07 09:45:04 +0800

    [TodoList][Austin Huang] refactor : throw Exception when delete todo

--- src/main/java/com/oocl/todolist/service/TodoService.java
+++ src/main/java/com/oocl/todolist/service/TodoService.java
@@ -6,12 +6,12 @@ import com.oocl.todolist.exception.IllegalOperationException;
 import com.oocl.todolist.exception.NoSuchDataException;
 import com.oocl.todolist.mapper.TodoMapper;
 import com.oocl.todolist.model.Todo;
-import org.springframework.beans.BeanUtils;
 import org.springframework.stereotype.Service;
 import org.springframework.util.StringUtils;
 
 import java.util.List;
 import java.util.Objects;
+import java.util.Optional;
 
 @Service
 public class TodoService {
@@ -45,8 +45,10 @@ public class TodoService {
     }
 
     public void deleteTodo(String id) {
-        if(!StringUtils.isEmpty(id)) {
-            todoRepository.deleteById(id);
+        Todo todo = todoRepository.findById(id).orElse(null);
+        if(Objects.isNull(todo)) {
+            throw new NoSuchDataException();
         }
+        todoRepository.deleteById(id);
     }
 }
